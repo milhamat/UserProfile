35c00383dc08f3028dcae8b1ae19c45abe27ad23
milhamat <M477ashiddiq@gmail.com>
2021-03-06 12:04:18 +0700

    Initial Commit

---
+++ UserProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserProfile
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 06/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
